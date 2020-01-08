f562c426b894c7b3af4c5032b3208689c855f129
aldy-deptech <aldy@deptechdigital.com>
2020-01-08 17:14:54 +0700

    aldy 8 jan

---
+++ app/src/main/java/com/unilever/go/wallsopsi1/Controller/cctv.java
@@ -0,0 +1,13 @@
+package com.unilever.go.wallsopsi1.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cctv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cctv);
+    }
+}
